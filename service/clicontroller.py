54af821f6e5a6e951b0c3f064b05886d732f8ed4
ont-tester <ont-tester@tester.com>
2018-12-19 13:07:36 +0800

    update cli api

--- service/clicontroller.py
+++ service/clicontroller.py
@@ -54,24 +54,6 @@ class CLIController:
         self.logfile.write("expect \"" + str + "\"" + "\n")
         pass
 
-    def version(self):
-        self.writesend("version")
-        pass
-
-    # help  帮助菜单
-    def help(self):
-        self.writesend("help")
-        pass
-
-    # clear 清除屏幕
-    def clear(self):
-        pass
-
-    # exit  退出程序
-    def exit(self):
-        self.writesend("exit")
-        pass
-
     def kill(self):
         os.system("kill -9 dotnet")
 
@@ -119,6 +101,25 @@ class CLIController:
                     return (False, key, msg)
         return (True, "", msg)
 
+    def version(self, exceptfunc=None):
+        self.writesend("version")
+        self.stepexceptfuncs["[version]-" + str(self.stepindex)] = exceptfunc
+        self.stepindex += 1
+
+    # help  帮助菜单
+    def help(self, exceptfunc=None):
+        self.writesend("help")
+        self.stepexceptfuncs["[help]-" + str(self.stepindex)] = exceptfunc
+        self.stepindex += 1
+
+    # clear 清除屏幕
+    def clear(self):
+        self.writesend("clear")
+
+    # exit  退出程序
+    def exit(self):
+        self.writesend("exit")
+
     # create wallet <path>  创建钱包文件
     def create_wallet(self, filepath, password, exceptfunc=None):
         self.writeline("echo '[START CLI COMMAND]-[create wallet]-" + str(self.stepindex) + "'")
@@ -145,8 +146,11 @@ class CLIController:
         self.stepindex += 1
 
     # upgrade wallet <path> 升级旧版钱包文件
-    def upgrade_wallet(self, filepath):
-        pass
+    def upgrade_wallet(self, filepath, exceptfunc=None):
+        self.writesend("upgrade wallet " + filepath)
+        # register except function
+        self.stepexceptfuncs["[upgrade wallet]-" + str(self.stepindex)] = exceptfunc
+        self.stepindex += 1
 
     # rebuild index 重建钱包索引  需要打开钱包
     # 重建钱包索引。为什么要重建钱包索引，重建钱包索引有什么用？
@@ -157,20 +161,32 @@ class CLIController:
     # 假如由于种种原因，钱包中的某笔交易未确认，这时资产已经从钱包中扣除，
     # 但并未经过整个区块链网络的确认。如果想删掉这笔未确认的交易使钱包中的资产正常显示也需要重建钱包索引。
     # 新创建的钱包不用重建钱包索引，只有要导入私钥或者钱包中资产显示异常时才需要重建钱包索引。
-    def rebuild_index(self, filepath):
-        pass
+    def rebuild_index(self, exceptfunc=None):
+        self.writesend("rebuild index")
+        # register except function
+        self.stepexceptfuncs["[rebuild index]-" + str(self.stepindex)] = exceptfunc
+        self.stepindex += 1
 
     # list address  列出钱包中的所有账户  需要打开钱包
-    def list_address(self):
-        pass
+    def list_address(self, exceptfunc=None):
+        self.writesend("list address")
+        # register except function
+        self.stepexceptfuncs["[list address]-" + str(self.stepindex)] = exceptfunc
+        self.stepindex += 1
 
     # list asset    列出钱包中的所有资产  需要打开钱包
-    def list_asset(self):
-        pass
+    def list_asset(self, exceptfunc=None):
+        self.writesend("list asset")
+        # register except function
+        self.stepexceptfuncs["[list asset]-" + str(self.stepindex)] = exceptfunc
+        self.stepindex += 1
 
     # list key  列出钱包中的所有公钥  需要打开钱包
-    def list_key(self):
-        pass
+    def list_key(self, exceptfunc=None):
+        self.writesend("list key")
+        # register except function
+        self.stepexceptfuncs["[list key]-" + str(self.stepindex)] = exceptfunc
+        self.stepindex += 1
 
     # show utxo [id|alias]  列出钱包中指定资产的 UTXO 需要打开钱包
     # examples:
@@ -183,30 +199,51 @@ class CLIController:
     # 3. neo>show utxo 025d82f7b00a9ff1cfe709abe3c4741a105d067178e645bc3ebad9bc79af47d4
     #    8674c38082e59455cf35cee94a5a1f39f73b617b3093859aa199c756f7900f1f:0
     #    total: 1 UTXOs
-    def show_utxo(self, id_alias=None):
-        pass
+    def show_utxo(self, id_alias=None, exceptfunc=None):
+        if id_alias is None:
+            self.writesend("show utxo")
+        else:
+            self.writesend("show utxo " + id_alias)
+        # register except function
+        self.stepexceptfuncs["[list key]-" + str(self.stepindex)] = exceptfunc
+        self.stepindex += 1
 
     # show gas  列出钱包中的所有可提取及不可提取的 GAS   需要打开钱包
     # examples:
     # unavailable: 133.024
     # available: 10.123
-    def show_gas(self):
-        pass
+    def show_gas(self, exceptfunc=None):
+        self.writesend("show gas")
+        # register except function
+        self.stepexceptfuncs["[show gas]-" + str(self.stepindex)] = exceptfunc
+        self.stepindex += 1
 
     # claim gas 提取钱包中的所有可提取的 GAS    需要打开钱包
-    def claim_gas(self):
-        pass
+    def claim_gas(self, exceptfunc=None):
+        self.writesend("claim gas")
+        # register except function
+        self.stepexceptfuncs["[claim gas]-" + str(self.stepindex)] = exceptfunc
+        self.stepindex += 1
 
     # create address [n=1]  创建地址 / 批量创建地址   需要打开钱包
-    def create_address(self, n=None):
-        pass
+    def create_address(self, n=None, exceptfunc=None):
+        if n is None:
+            self.writesend("create address")
+        else:
+            self.writesend("create address " + str(n))
+        # register except function
+        self.stepexceptfuncs["[create address]-" + str(self.stepindex)] = exceptfunc
+        self.stepindex += 1
 
     # import key <wif|path> 导入私钥 / 批量导入私钥   需要打开钱包
     # examples:
     # import key L4zRFphDJpLzXZzYrYKvUoz1LkhZprS5pTYywFqTJT2EcmWPPpPH
     # import key key.txt
-    def import_key(self, wif_path):
-        pass
+    def import_key(self, wif_path, exceptfunc=None):
+        self.writesend("import key " + str(wif_path))
+        # register except function
+        self.stepexceptfuncs["[import key]-" + str(self.stepindex)] = exceptfunc
+        self.stepindex += 1
 
     # export key [address] [path]   导出私钥    需要打开钱包
     # examples:
@@ -214,50 +251,95 @@ class CLIController:
     # export key AeSHyuirtXbfZbFik6SiBW2BEj7GK3N62b
     # export key key.txt
     # export key AeSHyuirtXbfZbFik6SiBW2BEj7GK3N62b key.txt
-    def export_key(self, address=None, path=None):
-        pass
+    def export_key(self, address=None, path=None, exceptfunc=None):
+        addressstr = ""
+        pathstr = ""
+        if address is not None:
+            addressstr = address
+        if path is not None:
+            pathstr = path
+        self.writesend("export key " + str(addressstr) + " " + str(pathstr))
+        # register except function
+        self.stepexceptfuncs["[export key]-" + str(self.stepindex)] = exceptfunc
+        self.stepindex += 1
 
     # send <id|alias> <address> <value>|all [fee=0] 向指定地址转账 参数分别为：资产 ID，对方地址，转账金额，手续费   需要打开钱包
     # examples:
     # 1. send c56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b AeSHyuirtXbfZbFik6SiBW2BEj7GK3N62b 100
     # 2. send neo AeSHyuirtXbfZbFik6SiBW2BEj7GK3N62b 100
-    def send(self, id_alias, address, value, fee=0):
-        pass
+    def send(self, id_alias, address, value, fee=0, exceptfunc=None):
+        self.writesend("send " + str(id_alias) + " " + str(address) + " " + str(value) + " " + str(fee))
+        # register except function
+        self.stepexceptfuncs["[send]-" + str(self.stepindex)] = exceptfunc
+        self.stepindex += 1
 
     # import multisigaddress m pubkeys...   创建多方签名合约    需要打开钱包
     # examples:
     # import multisigaddress 1 037ebe29fff57d8c177870e9d9eecb046b27fc290ccbac88a0e3da8bac5daa630d 03b34a4be80db4a38f62bb41d63f9b1cb664e5e0416c1ac39db605a8e30ef270cc
-    def import_multisigaddress(self, m, pubkeys):
-        pass
+    def import_multisigaddress(self, m, pubkeys, exceptfunc=None):
+        self.writesend("import multisigaddress " + str(m) + " " + " ".join(pubkeys))
+        # register except function
+        self.stepexceptfuncs["[import multisigaddress]-" + str(self.stepindex)] = exceptfunc
+        self.stepindex += 1
 
     # sign <jsonObjectToSign>   签名 参数为：记录交易内容的 json 字符串 需要打开钱包
-    def sign(self, jsonobj):
-        pass
+    def sign(self, jsonobj, exceptfunc=None):
+        self.writesend("sign " + jsonobj)
+        # register except function
+        self.stepexceptfuncs["[sign]-" + str(self.stepindex)] = exceptfunc
+        self.stepindex += 1
 
     # relay <jsonObjectToSign>  广播 参数为：记录交易内容的 json 字符串 需要打开钱包
-    def relay(self, jsonobj):
-        pass
+    def relay(self, jsonobj, exceptfunc=None):
+        self.writesend("relay " + jsonobj)
+        # register except function
+        self.stepexceptfuncs["[relay]-" + str(self.stepindex)] = exceptfunc
+        self.stepindex += 1
 
     # show state    显示当前区块链同步状态
-    def show_state(self):
-        pass
+    def show_state(self, exceptfunc=None):
+        self.writesend("show state")
+        # register except function
+        self.stepexceptfuncs["[show state]-" + str(self.stepindex)] = exceptfunc
+        self.stepindex += 1
 
     # show node 显示当前已连接的节点地址和端口
-    def show_node(self):
-        pass
+    def show_node(self, exceptfunc=None):
+        self.writesend("show node")
+        # register except function
+        self.stepexceptfuncs["[show node]-" + str(self.stepindex)] = exceptfunc
+        self.stepindex += 1
 
     # show pool 显示内存池中的交易（这些交易处于零确认的状态）
-    def show_pool(self):
-        pass
+    def show_pool(self, exceptfunc=None):
+        self.writesend("show pool")
+        # register except function
+        self.stepexceptfuncs["[show pool]-" + str(self.stepindex)] = exceptfunc
+        self.stepindex += 1
 
     # export blocks [path=chain.acc]    导出全部区块数据，导出的结果可以用作离线同步
-    def export_all_blocks(self, path=None):
-        pass
+    def export_all_blocks(self, path=None, exceptfunc=None):
+        if path is None:
+            self.writesend("export blocks")
+        else:
+            self.writesend("export blocks " + path)
+        # register except function
+        self.stepexceptfuncs["[export blocks]-" + str(self.stepindex)] = exceptfunc
+        self.stepindex += 1
 
     # export blocks <start> [count] 从指定区块高度导出指定数量的区块数据，导出的结果可以用作离线同步
-    def export_blocks(self, start, count=None):
-        pass
+    def export_blocks(self, start, count=None, exceptfunc=None):
+        if count is None:
+            self.writesend("export blocks " + str(start))
+        else:
+            self.writesend("export blocks " + str(start) + " " + str(count))
+        # register except function
+        self.stepexceptfuncs["[export blocks]-" + str(self.stepindex)] = exceptfunc
+        self.stepindex += 1
 
     # start consensus   启动共识
-    def start_consensus(self):
-        pass
+    def start_consensus(self, exceptfunc=None):
+        self.writesend("start consensus")
+        # register except function
+        self.stepexceptfuncs["[start consensus]-" + str(self.stepindex)] = exceptfunc
+        self.stepindex += 1
